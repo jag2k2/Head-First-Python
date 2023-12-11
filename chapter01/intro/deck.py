f07cdd00073ab9ae5230fb15d130e3cd6a10cbf3
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 14:20:06 -0600

    added comments

--- chapter01/intro/deck.py
+++ chapter01/intro/deck.py
@@ -1,8 +1,8 @@
 import random
 
-suits = ["Clubs", "Spades", "Hearts", "Ace"]
-faces = ["Jack", "Queen", "King", "Ace"]
-numbered = [2,3,4,5,6,7,8,9,10]
+suits = ["Clubs", "Spades", "Hearts", "Ace"]        # python list
+faces = ["Jack", "Queen", "King", "Ace"]            # python list
+numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10]             # python list
 
 def draw ():
     the_suit = random.choice(suits)
@@ -10,5 +10,5 @@ def draw ():
     return the_card, "of", the_suit
 
 if __name__ == "__main__":
-    for _ in range (5):
+    for _ in range (5):         # _ is python's default variable 
         print(draw())
