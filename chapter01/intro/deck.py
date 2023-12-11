8ccfc93ccff9ab78415c2447369afc299f670a4f
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 14:23:04 -0600

    added more comments

--- chapter01/intro/deck.py
+++ chapter01/intro/deck.py
@@ -1,13 +1,13 @@
 import random
 
-suits = ["Clubs", "Spades", "Hearts", "Ace"]        # python list
-faces = ["Jack", "Queen", "King", "Ace"]            # python list
-numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10]             # python list
+suits = ["Clubs", "Spades", "Hearts", "Ace"]        # python list [] - mutable
+faces = ["Jack", "Queen", "King", "Ace"]            # python list [] - mutable
+numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10]             # python list [] - mutable
 
 def draw ():
     the_suit = random.choice(suits)
     the_card = random.choice(faces + numbered)
-    return the_card, "of", the_suit
+    return the_card, "of", the_suit                 # returns a python tuple () - immutable
 
 if __name__ == "__main__":
     for _ in range (5):         # _ is python's default variable 
