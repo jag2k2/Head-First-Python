a9c7be9140b7ff3eac1a05ff949a99ec61d59f9f
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 21:19:14 -0600

    embracing tuple returns

--- chapter04/swim_reader.py
+++ chapter04/swim_reader.py
@@ -2,13 +2,13 @@ from coach.src import swimclub
 
 filename = "Darius-13-100m-Fly.txt"
 
-swimmer, age, distance, stroke = swimclub.get_swimmer_info(filename)
-converts, avg_time = swimclub.read_swim_data(filename)
+swimmer_info = swimclub.get_swimmer_info(filename)
+swim_data = swimclub.read_swim_data(filename)
 
-print(swimmer)
-print(age)
-print(distance)
-print(stroke)
+print(swimmer_info[0])
+print(swimmer_info[1])
+print(swimmer_info[2])
+print(swimmer_info[3])
 
-print(converts)
-print(avg_time)
+print(swim_data[0])
+print(swim_data[1])
