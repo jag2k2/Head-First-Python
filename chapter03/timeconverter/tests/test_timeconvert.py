162635d1c3d84651a30772c813f241edf34a3514
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 19:14:33 -0600

    implemented tests

--- chapter03/timeconverter/tests/test_timeconvert.py
+++ chapter03/timeconverter/tests/test_timeconvert.py
@@ -1,7 +1,11 @@
 from src import timeconvert
 
 def test_string_to_centaseconds():
-    assert True
+    time = "1:30.96"
+    expected_centasecs = 9096
+    assert timeconvert.convert_time_to_centaseconds(time) == expected_centasecs
 
 def test_centaseconds_to_string():
-    assert True
+    centasecs = 9096
+    expected_time = "1:30.96"
+    assert timeconvert.convert_centaseconds_to_time(centasecs) == expected_time
