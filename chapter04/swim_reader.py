6606e0db6b8bb7fe4fd43a6c4349ad809d07a1bc
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 21:12:46 -0600

    added simclub module

--- chapter04/swim_reader.py
+++ chapter04/swim_reader.py
@@ -1,17 +1,14 @@
-from pathlib import Path
-from statistics import mean
-from timeconverter.src import timeconvert
+from coach.src import swimclub
 
-FN = "Darius-13-100m-Fly.txt"
-FOLDER = Path("../swimdata/")
+filename = "Darius-13-100m-Fly.txt"
 
-with open (FOLDER / FN) as file:
-    lines = file.readlines()
-    contents = lines[0].strip()
-    times = contents.split(",")
-    converts = []
-    for time in times:
-        converts.append(timeconvert.convert_time_to_centaseconds(time))
-    print(converts)
-    avg_time = mean(converts)
-    print(timeconvert.convert_centaseconds_to_time(avg_time))
+swimmer, age, distance, stroke = swimclub.get_swimmer_info(filename)
+converts, avg_time = swimclub.read_swim_data(filename)
+
+print(swimmer)
+print(age)
+print(distance)
+print(stroke)
+
+print(converts)
+print(avg_time)
