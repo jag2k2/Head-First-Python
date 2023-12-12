77693d226ff98f973ca21b9a32af15615b6718da
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 19:16:43 -0600

    deleted redundant function

--- chapter03/swim_reader.py
+++ chapter03/swim_reader.py
@@ -5,12 +5,6 @@ from timeconverter.src import timeconvert
 FN = "Darius-13-100m-Fly.txt"
 FOLDER = Path("./swimdata/")
 
-# def convert_time_to_centaseconds(time: str) -> int:
-#     minutes, rest = time.split(":")
-#     seconds, hundredths = rest.split(".")
-#     centaseconds = int(minutes)*6000 + int(seconds)*100 + int(hundredths)
-#     return centaseconds
-
 with open (FOLDER / FN) as file:
     lines = file.readlines()
     contents = lines[0].strip()
