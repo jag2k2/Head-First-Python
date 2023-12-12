aa93c892ccaafc178507f0bb0668d5c4d2b8092f
Jeff Tipps <jeff.tipps@ni.com>
2023-12-11 19:33:37 -0600

    restrurcturing swim data to share between mult chapters

---
+++ chapter04/timeconverter/src/timeconvert.py
@@ -0,0 +1,12 @@
+def convert_time_to_centaseconds(time: str) -> int:
+    minutes, rest = time.split(":")
+    seconds, hundredths = rest.split(".")
+    centaseconds = int(minutes)*6000 + int(seconds)*100 + int(hundredths)
+    return centaseconds
+
+def convert_centaseconds_to_time(centasecs: int) -> str:
+    mins_secs, hundredths = str(round(centasecs / 100, 2)).split(".")
+    mins_secs = int(mins_secs)
+    minutes = mins_secs // 60
+    seconds = mins_secs - minutes*60
+    return str(minutes) + ":" + str(seconds) + "." + hundredths
